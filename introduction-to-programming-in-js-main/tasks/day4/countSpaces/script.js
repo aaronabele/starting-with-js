9945a7f2518979d019d5507ab30a8ee05d5acbd4
Aaron Abele <aaronabele.coding@gmail.com>
2022-11-29 20:58:48 +0100

    adds files and solutions

--- introduction-to-programming-in-js-main/tasks/day4/countSpaces/script.js
+++ introduction-to-programming-in-js-main/tasks/day4/countSpaces/script.js
@@ -1,3 +1,1 @@
-function countSpaces() {
-  // your code
-}
+function countSpaces() {}
--- introduction-to-programming-in-js-main/tasks/day4/evenLettersToUpperCase/script.js
+++ introduction-to-programming-in-js-main/tasks/day4/evenLettersToUpperCase/script.js
@@ -1,3 +1,15 @@
-function evenLettersToUpperCase() {
-  // your code
+function evenLettersToUpperCase(str) {
+  if (str && typeof str === "string") {
+    str = str.toLocaleLowerCase();
+
+    const strToArray = str.split("");
+
+    for (i = 0; i < strToArray.length; i++) {
+      if (i % 2 === 1) {
+        strToArray[i] = strToArray[i].toLocaleUpperCase();
+      }
+    }
+    console.log(strToArray.join(""));
+    return strToArray.join("");
+  }
 }
