d19a4f400a84378835ec2de30eddf218ed71bee9
Aaron Abele <aaronabele.coding@gmail.com>
2022-11-29 21:22:57 +0100

    adds solution countSpaces

--- introduction-to-programming-in-js-main/tasks/day4/countSpaces/script.js
+++ introduction-to-programming-in-js-main/tasks/day4/countSpaces/script.js
@@ -1,1 +1,9 @@
-function countSpaces() {}
+function countSpaces(str) {
+  if (str && typeof str === "string") {
+    let count = str.split(" ").length - 1;
+    console.log(count);
+    return count;
+  } else {
+    return undefined;
+  }
+}
